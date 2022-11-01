c1d947eb54f5ec707eebe9b48ef3763fac64814a
Polina Cherkasova <polinach@google.com>
2022-10-31 18:55:36 -0700

    -

--- lib/leak_analysis.dart
+++ lib/leak_analysis.dart
@@ -2,4 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+/// Lybrary to be used by devtools to analyse the collected leaks.
+///
+/// Should not be used in the application itself.
+library leak_analysis;
 
+export 'src/model.dart';
