7629e3257ef67a57d5325cad5aff40f2d51d01e0
Polina Cherkasova <polinach@google.com>
2022-10-31 18:47:51 -0700

    -

---
+++ lib/src/_util.dart
@@ -0,0 +1,25 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Compares two maps for element-by-element equality.
+///
+/// Copied from
+/// https://github.com/flutter/flutter/blob/37b72342b0ce86fbfc238a9d43e524608b89af3a/packages/flutter/lib/src/foundation/collections.dart#L22
+bool mapEquals<T, U>(Map<T, U>? a, Map<T, U>? b) {
+  if (a == null) {
+    return b == null;
+  }
+  if (b == null || a.length != b.length) {
+    return false;
+  }
+  if (identical(a, b)) {
+    return true;
+  }
+  for (final T key in a.keys) {
+    if (!b.containsKey(key) || b[key] != a[key]) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ test/model_test.dart
@@ -0,0 +1,74 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:leak_tracker/leak_tracker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('$Leaks serializes.', () {
+    // final task = NotGCedAnalyzerTask(
+    //   reports: [
+    //     LeakReport(
+    //       type: 'type',
+    //       details: ['details'],
+    //       code: 2,
+    //     )
+    //   ],
+    //   heap: AdaptedHeapData(
+    //     [
+    //       AdaptedHeapObject(
+    //         heapClass: HeapClassName(
+    //           className: 'class',
+    //           library: 'library',
+    //         ),
+    //         references: [2, 3, 4],
+    //         code: 6,
+    //         shallowSize: 1,
+    //       ),
+    //     ],
+    //     rootIndex: 0,
+    //   ),
+    // );
+
+    // final json = task.toJson();
+
+    // expect(
+    //   jsonEncode(json),
+    //   jsonEncode(NotGCedAnalyzerTask.fromJson(json).toJson()),
+    // );
+  });
+
+  test('$LeakSummary serializes.', () {
+    // final task = NotGCedAnalyzerTask(
+    //   reports: [
+    //     LeakReport(
+    //       type: 'type',
+    //       details: ['details'],
+    //       code: 2,
+    //     )
+    //   ],
+    //   heap: AdaptedHeapData(
+    //     [
+    //       AdaptedHeapObject(
+    //         heapClass: HeapClassName(
+    //           className: 'class',
+    //           library: 'library',
+    //         ),
+    //         references: [2, 3, 4],
+    //         code: 6,
+    //         shallowSize: 1,
+    //       ),
+    //     ],
+    //     rootIndex: 0,
+    //   ),
+    // );
+
+    // final json = task.toJson();
+
+    // expect(
+    //   jsonEncode(json),
+    //   jsonEncode(NotGCedAnalyzerTask.fromJson(json).toJson()),
+    // );
+  });
+}
