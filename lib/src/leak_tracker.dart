33f9dc8350a1a6f8180278337b16b56370e5fc43
Polina Cherkasova <polinach@google.com>
2022-11-09 16:58:51 -0800

    -

---
+++ lib/src/leak_tracker.dart
@@ -0,0 +1,44 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// Enables leak tracking for the application.
+///
+/// See usage guidance at https://github.com/dart-lang/leak_tracker.
+void enableLeakTracking() => throw UnimplementedError();
+
+/// Dispatches an object event to the leak tracker.
+///
+/// Consumes the MemoryAllocations event format:
+/// https://github.com/flutter/flutter/blob/a479718b02a818fb4ac8d4900bf08ca389cd8e7d/packages/flutter/lib/src/foundation/memory_allocations.dart#L51
+void dispatchObjectEvent(Map<Object, Map<String, Object>> event) =>
+    throw UnimplementedError();
+
+/// Dispatches object creation to the leak tracker.
+///
+/// Use [info] to provide additional information, that may help in leek troubleshooting.
+void dispatchObjectCreated({
+  required String library,
+  required String className,
+  required Object object,
+  Map<String, dynamic>? info,
+}) =>
+    throw UnimplementedError();
+
+/// Dispatches object disposal to the leak tracker.
+///
+/// See [dispatchObjectCreated] for parameters documentation.
+void dispatchObjectDisposed({
+  required Object object,
+  Map<String, dynamic>? info,
+}) =>
+    throw UnimplementedError();
+
+/// Dispatches additional object information to the leak tracker.
+///
+/// See [dispatchObjectCreated] for parameters documentation.
+void dispatchObjectTrace({
+  required Object object,
+  Map<String, dynamic>? info,
+}) =>
+    throw UnimplementedError();
