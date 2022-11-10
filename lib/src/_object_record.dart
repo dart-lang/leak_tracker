d4bbf48d7ae9f214929b9869408cfbd021307fe3
Polina Cherkasova <polinach@google.com>
2022-11-10 15:47:54 -0800

    Update _object_record.dart

--- lib/src/_object_record.dart
+++ lib/src/_object_record.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import '_gcCounter.dart';
+import '_gc_counter.dart';
 import '_primitives.dart';
 import 'model.dart';
 
