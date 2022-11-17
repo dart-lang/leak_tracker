c72bef857ee9b50547f2f7f958f96428dcf19551
Polina Cherkasova <polinach@google.com>
2022-11-17 15:05:41 -0800

    -

--- test/test_infra/generated.dart
+++ test/test_infra/generated.dart
@@ -2,14 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker/src/_object_tracker.dart';
+import 'package:mockito/annotations.dart';
 
-import '../test_infra/generated.mocks.dart';
-
-void main() {
-  test('$ObjectTracker consumes Flutter SDK instrumentation.', () {
-    final tracker = MockObjectTracker();
-    expect(tracker, isNotNull);
-  });
-}
+@GenerateNiceMocks([MockSpec<ObjectTracker>()])
+void main() {}
