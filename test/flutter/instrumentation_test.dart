5cfb286d33316c62158d40b15b2d14872887ad76
Polina Cherkasova <polinach@google.com>
2022-11-17 15:28:33 -0800

    Update instrumentation_test.dart

--- test/flutter/instrumentation_test.dart
+++ test/flutter/instrumentation_test.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:leak_tracker/src/_dispatcher.dart';
 import 'package:leak_tracker/src/_object_tracker.dart';
 
 import '../test_infra/generated.mocks.dart';
@@ -11,5 +13,8 @@ void main() {
   test('$ObjectTracker consumes Flutter SDK instrumentation.', () {
     final tracker = MockObjectTracker();
     expect(tracker, isNotNull);
+
+    MemoryAllocations.instance
+        .addListener((event) => dispatchObjectEvent(event.toMap(), tracker));
   });
 }
