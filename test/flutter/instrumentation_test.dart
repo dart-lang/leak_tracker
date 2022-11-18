fd4f110699046743e6e9128582c4483cb90f9604
Polina Cherkasova <polinach@google.com>
2022-11-17 16:01:30 -0800

    -

--- test/flutter/instrumentation_test.dart
+++ test/flutter/instrumentation_test.dart
@@ -6,15 +6,27 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker/src/_dispatcher.dart';
 import 'package:leak_tracker/src/_object_tracker.dart';
+import 'package:leak_tracker/src/leak_tracker_model.dart';
 
 import '../test_infra/generated.mocks.dart';
 
 void main() {
   test('$ObjectTracker consumes Flutter SDK instrumentation.', () {
     final tracker = MockObjectTracker();
-    expect(tracker, isNotNull);
+    //when(tracker.startTracking(object, context: context, trackedClass: trackedClass))
 
     MemoryAllocations.instance
         .addListener((event) => dispatchObjectEvent(event.toMap(), tracker));
   });
+}
+
+class MockObjectTracker extends ObjectTracker {
+  MockObjectTracker() : super(LeakTrackingConfiguration());
+
+  @override
+  void startTracking(
+    Object object, {
+    required Map<String, dynamic>? context,
+    required String trackedClass,
+  }) {}
 }
