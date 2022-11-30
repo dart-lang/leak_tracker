087dc6e2a55df79e55c4ea196c1676ae3452b5fe
Polina Cherkasova <polinach@google.com>
2022-11-29 16:01:27 -0800

    Update _leak_checker_test.dart

--- test/dart/_leak_checker_test.dart
+++ test/dart/_leak_checker_test.dart
@@ -2,10 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:leak_tracker/leak_tracker.dart';
 import 'package:leak_tracker/src/_leak_checker.dart';
 import 'package:leak_tracker/src/_model.dart';
