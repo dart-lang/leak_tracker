c2e8a6f57e5860b53472329ccb99b49ab4f3b2f5
Polina Cherkasova <polina.c@live.com>
2022-12-06 16:55:56 -0800

    -

--- test/flutter/integration/README.md
+++
@@ -1,2 +0,0 @@
-This folder tests non-mocked public API of leak tracker.
-The tests can serve as examples for regression leak-testing for Flutter widgets.
--- test/flutter/integration_test.dart
+++ test/flutter/integration_test.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker/leak_tracker.dart';
-import 'package:test/test.dart';
 
 import '../test_infra/data/instrumented_classes.dart';
 import '../test_infra/helpers/gc.dart';
