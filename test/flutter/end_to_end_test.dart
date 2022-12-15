1d8f66456c14e7171a4044872b1663dba47774cf
Polina Cherkasova <polina.c@live.com>
2022-12-15 13:41:06 -0500

    Update end_to_end_test.dart

--- test/flutter/end_to_end_test.dart
+++ test/flutter/end_to_end_test.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker/leak_tracker.dart';
 
