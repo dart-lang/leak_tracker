f486867a65460b019cf0164afbc29806c06c4789
Polina Cherkasova <polinach@google.com>
2023-01-24 15:27:40 -0500

    - (#38)

--- lib/src/leak_tracker.dart
+++ lib/src/leak_tracker.dart
@@ -2,12 +2,13 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import '../leak_tracker.dart';
 import '_dispatcher.dart' as dispatcher;
 import '_leak_checker.dart';
 import '_object_tracker.dart';
 import '_primitives.dart';
 import 'devtools_integration/_registration.dart';
+import 'leak_tracker_model.dart';
+import 'shared_model.dart';
 
 final _objectTracker = ObjectRef<ObjectTracker?>(null);
 LeakChecker? _leakChecker;
