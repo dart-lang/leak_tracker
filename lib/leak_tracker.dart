b13fc654bedefdf292f3d2d6c5ee2679bb0d749d
Polina Cherkasova <polina.c@live.com>
2023-04-21 16:57:36 -0700

    Update leak_tracker.dart

--- lib/leak_tracker.dart
+++ lib/leak_tracker.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+export 'src/autosnapshotting/autosnapshotting.dart';
 export 'src/leak_detection/leak_tracker.dart';
 export 'src/leak_detection/leak_tracker_model.dart';
 export 'src/leak_detection/matchers.dart';
