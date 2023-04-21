2f8a96c622a842c22e2e8af52624f2814368d3d5
Polina Cherkasova <polina.c@live.com>
2023-04-21 15:58:08 -0700

    -

--- lib/leak_tracker.dart
+++ lib/leak_tracker.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/leak_tracker.dart';
-export 'src/leak_tracker_model.dart';
-export 'src/matchers.dart';
-export 'src/orchestration.dart';
+export 'src/leak_detection/leak_tracker.dart';
+export 'src/leak_detection/leak_tracker_model.dart';
+export 'src/leak_detection/matchers.dart';
+export 'src/leak_detection/orchestration.dart';
 export 'src/shared/shared_model.dart';
--- test/dart_test_infra/data/dart_classes.dart
+++ test/dart_test_infra/data/dart_classes.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:leak_tracker/src/leak_tracker.dart';
+import 'package:leak_tracker/src/leak_detection/leak_tracker.dart';
 
 class InstrumentedClass {
   InstrumentedClass() {
