69adc9c67f690c9706a7ed5bf4d068fa1c751f8d
Polina Cherkasova <polina.c@live.com>
2023-04-22 15:40:08 -0700

    -

--- more_examples/autosnapshotting/lib/main.dart
+++ more_examples/autosnapshotting/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,17 +32,17 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+final _allocations = <List<DateTime>>[];
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class _MyHomePageState extends State<MyHomePage> {
+  void _allocateMemory() {
+    _allocations.add(List.generate(10000, (_) => DateTime.now()));
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
+    final formatter = NumberFormat('#,##,000');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -50,19 +52,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              'Current RSS: ${formatter.format(ProcessInfo.currentRss)}',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: _allocateMemory,
+        tooltip: 'Allocate more memory',
         child: const Icon(Icons.add),
       ),
     );
