9bda46768f050e82ed8450348dfd1892e5f44e40
Polina Cherkasova <polinach@google.com>
2023-05-04 13:32:12 -0700

    Fix broken link. (#57)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 4.0.3
+
+* Fix broken documentation link.
+
 # 4.0.2
 
 * Improve documentation.
---
+++ test/dart_test_infra/utils.dart
@@ -0,0 +1,23 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+Future<String> loadPageHtmlContent(String url) async {
+  final request = await HttpClient().getUrl(Uri.parse(url));
+  final response = await request.close();
+
+  final completer = Completer<String>();
+  final content = StringBuffer();
+  response.transform(utf8.decoder).listen(
+    (data) {
+      content.write(data);
+    },
+    onDone: () => completer.complete(content.toString()),
+  );
+  await completer.future;
+  return content.toString();
+}
