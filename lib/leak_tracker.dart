d512188d0143bbe51c4fd464871dd7cccb275e89
Polina Cherkasova <polina.c@live.com>
2023-05-18 09:25:41 -0700

    -

--- lib/leak_tracker.dart
+++ lib/leak_tracker.dart
@@ -4,8 +4,8 @@
 
 export 'src/leak_tracking/leak_tracker.dart';
 export 'src/leak_tracking/leak_tracker_model.dart';
-export 'src/leak_tracking/matchers.dart';
 export 'src/leak_tracking/orchestration.dart';
 export 'src/shared/shared_model.dart';
+export 'src/testing/matchers.dart';
 export 'src/usage_tracking/model.dart';
 export 'src/usage_tracking/usage_tracking.dart';
