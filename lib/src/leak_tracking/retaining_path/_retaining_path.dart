52b328c99f593c959bcf6fa2ef9cd3abd4ec61e8
Polina Cherkasova <polina.c@live.com>
2023-05-19 09:35:56 -0700

    Update _retaining_path.dart

--- lib/src/leak_tracking/retaining_path/_retaining_path.dart
+++ lib/src/leak_tracking/retaining_path/_retaining_path.dart
@@ -11,15 +11,23 @@ import '_service.dart';
 import '_vm_service_wrapper.dart';
 
 Future<RetainingPath> obtainRetainingPath(Type type, int code) async {
-  return RetainingPath();
+  await _connect();
+
+  final fp = _ObjectFingerprint(type, code);
+  final targetId = await _targetId(fp);
+  if (targetId == null) {
+    throw Exception('Could not find object in heap');
+  }
+
+  return await _service.getRetainingPath(_isolateId, targetId, 100000);
 }
 
-late String isolateId;
-late VmServiceWrapper service;
-bool connected = false;
+late String _isolateId;
+late VmServiceWrapper _service;
+bool _connected = false;
 
-Future<void> connect() async {
-  if (connected) return;
+Future<void> _connect() async {
+  if (_connected) return;
 
   final info = await Service.getInfo();
   if (info.serverWebSocketUri == null) {
@@ -28,46 +36,33 @@ Future<void> connect() async {
     );
   }
 
-  service = await connectWithWebSocket(info.serverWebSocketUri!, (error) {
+  _service = await connectWithWebSocket(info.serverWebSocketUri!, (error) {
     throw error ?? Exception('Error connecting to service protocol');
   });
 
-  await service.getVersion();
-  await service.forEachIsolate((IsolateRef r) async {
+  await _service.getVersion();
+  await _service.forEachIsolate((IsolateRef r) async {
     if (r.name == 'main') {
-      isolateId = r.id!;
+      _isolateId = r.id!;
     }
   });
 
-  connected = true;
-}
-
-Future<RetainingPath> getRetainingPath(Object object) async {
-  await connect();
-
-  final fp = _ObjectFingerprint(object);
-  final targetId = await _targetId(fp);
-  if (targetId == null) {
-    throw Exception('Could not find object in heap');
-  }
-
-  return await service.getRetainingPath(isolateId, targetId, 100000);
+  _connected = true;
 }
 
 class _ObjectFingerprint {
-  _ObjectFingerprint(Object object)
-      : type = object.runtimeType.toString(),
-        code = identityHashCode(object);
-  final String type;
+  _ObjectFingerprint(this.type, this.code);
+
+  final Type type;
   final int code;
 }
 
 Future<String?> _targetId(_ObjectFingerprint object) async {
-  final classes = await findClasses(object.type);
+  final classes = await findClasses(object.type.toString());
 
   for (final theClass in classes) {
     final instances =
-        (await service.getInstances(isolateId, theClass.id!, 10000000))
+        (await _service.getInstances(_isolateId, theClass.id!, 10000000))
                 .instances ??
             <ObjRef>[];
     final result = instances.firstWhereOrNull(
@@ -81,7 +76,7 @@ Future<String?> _targetId(_ObjectFingerprint object) async {
 }
 
 Future<List<ClassRef>> findClasses(String runtimeClassName) async {
-  final classes = await service.getClassList(isolateId);
+  final classes = await _service.getClassList(_isolateId);
   return classes.classes
           ?.where((ref) => runtimeClassName == ref.name)
           .toList() ??
