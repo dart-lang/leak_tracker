7fbb3174d4bf94baab571a11f6468c02227ac286
Polina Cherkasova <polina.c@live.com>
2023-05-25 09:47:57 -0700

    Create end_to_ent_test.dart

---
+++ test/dart_debug/leak_tracking/end_to_ent_test.dart
@@ -0,0 +1,45 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:leak_tracker/leak_tracker.dart';
+import 'package:leak_tracker/testing.dart';
+
+import 'package:test/test.dart';
+
+import '../../dart_test_infra/data/dart_classes.dart';
+
+/// Tests for non-mocked public API of leak tracker.
+void main() {
+  tearDown(() => disableLeakTracking());
+
+  test('Retaining path for not GCed object reported.', () async {
+    late LeakTrackedClass notGCedObject;
+    final leaks = await withLeakTracking(
+      () async {
+        notGCedObject = LeakTrackedClass();
+        // Dispose reachable instance.
+        notGCedObject.dispose();
+      },
+      shouldThrowOnLeaks: false,
+      leakDiagnosticConfig: const LeakDiagnosticConfig(
+        collectRetainingPathForNonGCed: true,
+      ),
+    );
+
+    expect(leaks.total, 1);
+    expect(
+      () => expect(leaks, isLeakFree),
+      throwsA(
+        predicate((e) {
+          print(e);
+          return true;
+        }),
+      ),
+    );
+
+    final theLeak = leaks.notGCed.first;
+    expect(theLeak.trackedClass, contains(LeakTrackedClass.library));
+    expect(theLeak.trackedClass, contains('$LeakTrackedClass'));
+  });
+}
