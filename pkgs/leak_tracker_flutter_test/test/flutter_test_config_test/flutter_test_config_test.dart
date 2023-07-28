624ce261bb31eb0ab690c78aaeff031f7451402b
Polina Cherkasova <polina.c@live.com>
2023-07-28 09:18:05 -0700

    Update flutter_test_config_test.dart

--- pkgs/leak_tracker_flutter_test/test/flutter_test_config_test/flutter_test_config_test.dart
+++ pkgs/leak_tracker_flutter_test/test/flutter_test_config_test/flutter_test_config_test.dart
@@ -2,22 +2,23 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: avoid_redundant_argument_values
-
 import 'package:flutter_test/flutter_test.dart';
 
 import '../test_infra/flutter_classes.dart';
 import '../test_infra/leak_tracking_in_flutter.dart';
 
+const test1_tracking_on = 'test1, tracking-on';
+const test2_tracking_off = 'test2, tracking-off';
+
 /// Tests for non-mocked public API of leak tracker.
 ///
 /// For this tests `expect` happens in flitter_test_config.dart.
 void main() {
-  testWidgetsWithLeakTracking('test1, tracking-on', (widgetTester) async {
+  testWidgetsWithLeakTracking(test1_tracking_on, (widgetTester) async {
     await widgetTester.pumpWidget(StatelessLeakingWidget());
   });
 
-  testWidgets('test2, tracking-off', (widgetTester) async {
+  testWidgets(test2_tracking_off, (widgetTester) async {
     await widgetTester.pumpWidget(StatelessLeakingWidget());
   });
 }
