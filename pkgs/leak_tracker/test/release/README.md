6f54f5a6ac2401e9007e3d5e431634a538c49d4c
Polina Cherkasova <polinach@google.com>
2023-08-01 13:38:14 -0700

    Start vm service when needed. (#112)

--- pkgs/leak_tracker/test/release/README.md
+++
@@ -1,1 +0,0 @@
-Bots run tests in this folder with 'dart test', where asserts are on, but VM service is unavailable.
--- pkgs/leak_tracker/test/tests/leak_tracking/end_to_end_test.dart
+++ pkgs/leak_tracker/test/tests/leak_tracking/end_to_end_test.dart
@@ -8,9 +8,83 @@ import 'package:test/test.dart';
 
 import '../../test_infra/data/dart_classes.dart';
 
-/// Tests for non-mocked public API of leak tracker.
 void main() {
   tearDown(() => LeakTracking.stop());
+
+  for (var numberOfGcCycles in [1, defaultNumberOfGcCycles]) {
+    test('Passive leak tracking detects leaks, $numberOfGcCycles.', () async {
+      LeakTracking.start(
+        resetIfAlreadyStarted: true,
+        config: LeakTrackingConfig.passive(
+          numberOfGcCycles: numberOfGcCycles,
+        ),
+      );
+
+      expect(LeakTracking.isStarted, true);
+      expect(LeakTracking.phase.isPaused, false);
+
+      LeakingClass();
+      LeakingClass();
+      LeakingClass();
+
+      expect(LeakTracking.phase.isPaused, false);
+
+      await forceGC(fullGcCycles: defaultNumberOfGcCycles);
+      final leaks = await LeakTracking.collectLeaks();
+      LeakTracking.stop();
+
+      expect(leaks.notGCed, hasLength(3));
+      expect(leaks.notDisposed, hasLength(3));
+      expect(
+        () => expect(leaks, isLeakFree),
+        throwsA(isA<TestFailure>()),
+      );
+    });
+  }
+
+  test('Stack trace does not start with leak tracker calls', () async {
+    LeakTracking.start(
+      resetIfAlreadyStarted: true,
+      config: LeakTrackingConfig.passive(),
+    );
+
+    LeakTracking.phase = const PhaseSettings(
+      leakDiagnosticConfig: LeakDiagnosticConfig(
+        collectStackTraceOnDisposal: true,
+        collectStackTraceOnStart: true,
+      ),
+    );
+
+    LeakingClass();
+
+    await forceGC(fullGcCycles: defaultNumberOfGcCycles);
+    final leaks = await LeakTracking.collectLeaks();
+    LeakTracking.stop();
+
+    expect(leaks.notGCed, hasLength(1));
+    expect(leaks.notDisposed, hasLength(1));
+
+    try {
+      expect(leaks, isLeakFree);
+    } catch (error) {
+      const traceHeaders = ['start: >', 'disposal: >'];
+
+      final lines = error.toString().split('\n').asMap();
+
+      for (final header in traceHeaders) {
+        final headerInexes =
+            lines.keys.where((i) => lines[i]!.endsWith(header));
+        expect(headerInexes, isNotEmpty);
+        for (final i in headerInexes) {
+          if (i + 1 >= lines.length) continue;
+          final line = lines[i + 1]!;
+
+          const leakTrackerStackTraceFragment = '(package:leak_tracker/';
+          expect(line, isNot(contains(leakTrackerStackTraceFragment)));
+        }
+      }
+    }
+  });
 
   for (var numberOfGcCycles in [1, defaultNumberOfGcCycles]) {
     test(
