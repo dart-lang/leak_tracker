ffafe63797b713824dfcec12439a2328dcdc5848
Polina Cherkasova <polina.c@live.com>
2023-08-26 21:01:40 -0700

    -

---
+++ pkgs/leak_tracker_flutter_testing/test/tests/matchers_test.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:leak_tracker/leak_tracker.dart';
+import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
+
+class _TrackedClass {
+  _TrackedClass() {
+    MemoryAllocations.instance.dispatchObjectCreated(
+        library: 'library', className: '_TrackedClass', object: this);
+  }
+
+  void dispose() {
+    MemoryAllocations.instance.dispatchObjectDisposed(object: this);
+  }
+}
+
+void main() {
+  test('dispatchesMemoryEvents success', () {
+    expect(
+      ObjectLyfecycle(() => _TrackedClass().dispose(), _TrackedClass),
+      dispatchesMemoryEvents,
+    );
+  });
+
+  test('dispatchesMemoryEvents failure', () {
+    expect(
+      () => expect(
+        ObjectLyfecycle(() {}, _TrackedClass),
+        dispatchesMemoryEvents,
+      ),
+      throwsA(isA<TestFailure>()),
+    );
+  });
+}
