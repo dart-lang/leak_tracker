bdd95f04e8868ed21baaf22784405682e19c67db
Polina Cherkasova <polina.c@live.com>
2023-08-28 18:41:57 -0700

    Update _retaining_path_test.dart

--- pkgs/leak_tracker/test/tests/leak_tracking/retaining_path/_retaining_path_test.dart
+++ pkgs/leak_tracker/test/tests/leak_tracking/retaining_path/_retaining_path_test.dart
@@ -72,7 +72,7 @@ void main() {
     await Future.wait(obtainers);
 
     expect(
-      _logs.where((item) => item == 'Connecting to vm service protocol...'),
+      _logs.where((item) => item == 'Connecting to VM service protocol...'),
       hasLength(1),
     );
   });
