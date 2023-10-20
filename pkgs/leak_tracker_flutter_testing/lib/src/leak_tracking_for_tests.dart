ee39afbd146d174e72bff021310d0c3f335626e0
Polina Cherkasova <polina.c@live.com>
2023-10-20 16:18:35 -0700

    Update leak_tracking_for_tests.dart

--- pkgs/leak_tracker_flutter_testing/lib/src/leak_tracking_for_tests.dart
+++ pkgs/leak_tracker_flutter_testing/lib/src/leak_tracking_for_tests.dart
@@ -23,7 +23,7 @@ void _emptyLeakHandler(Leaks leaks) {}
 /// ```dart
 /// testWidgets(
 ///     'initialTimerDuration falls within limit',
-///     leakTracking: LeakTrackingForTests.ignored(),
+///     leakTracking: LeakTrackingForTests.withIgnoredAll(),
 ///     (WidgetTester tester) async {
 ///       ...
 /// ```
