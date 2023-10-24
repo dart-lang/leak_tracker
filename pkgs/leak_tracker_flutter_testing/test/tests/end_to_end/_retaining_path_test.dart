02858701ef40cc7b891b5bc63189f408cf7c27ab
Polina Cherkasova <polina.c@live.com>
2023-10-23 17:24:15 -0700

    Update _retaining_path_test.dart

--- pkgs/leak_tracker_flutter_testing/test/tests/end_to_end/_retaining_path_test.dart
+++ pkgs/leak_tracker_flutter_testing/test/tests/end_to_end/_retaining_path_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:leak_tracker/src/leak_tracking/_primitives/_retaining_path/_connection.dart';
 import 'package:leak_tracker/src/leak_tracking/_primitives/_retaining_path/_retaining_path.dart';
