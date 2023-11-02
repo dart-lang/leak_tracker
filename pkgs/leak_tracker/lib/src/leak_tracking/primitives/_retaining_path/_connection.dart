cfc2be214a387dbf4b89ef86ec2aa4510058351d
Devon Carew <devoncarew@google.com>
2023-11-02 15:34:11 -0700

    remove the dependency on package:web_socket_channel

--- pkgs/leak_tracker/lib/src/leak_tracking/primitives/_retaining_path/_connection.dart
+++ pkgs/leak_tracker/lib/src/leak_tracking/primitives/_retaining_path/_connection.dart
@@ -6,22 +6,19 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:vm_service/vm_service.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:vm_service/vm_service_io.dart';
 
 Future<Uri> _serviceUri() async {
   Uri? uri = (await Service.getInfo()).serverWebSocketUri;
-
   if (uri != null) return uri;
 
   uri = (await Service.controlWebServer(enable: true)).serverWebSocketUri;
+  if (uri != null) return uri;
 
-  if (uri == null) {
-    throw StateError(
-      'Could not start VM service. If you are running `flutter test`, pass the flag `--enable-vmservice`',
-    );
-  }
-
-  return uri;
+  throw StateError(
+    'Could not start VM service. '
+    'If you are running `flutter test`, pass the flag `--enable-vmservice`',
+  );
 }
 
 /// Connects to vm service protocol.
@@ -30,33 +27,10 @@ Future<Uri> _serviceUri() async {
 Future<VmService> connect() async {
   final uri = await _serviceUri();
 
-  final service = await _connectWithWebSocket(uri, _handleError);
-  await service.getVersion(); // Warming up and validating the connection.
+  final service = await vmServiceConnectUri(uri.toString());
 
-  return service;
-}
-
-void _handleError(Object? error) {
-  throw error ?? Exception('Unknown error');
-}
-
-Future<VmService> _connectWithWebSocket(
-  Uri uri,
-  void Function(Object? error) onError,
-) async {
-  final ws = WebSocketChannel.connect(uri);
-  final stream = ws.stream.handleError(onError);
-  final service = VmService(
-    stream,
-    (String message) {
-      ws.sink.add(message);
-    },
-  );
-
-  if (ws.closeCode != null) {
-    onError(ws.closeReason);
-    return service;
-  }
+  // Warming up and validating the connection.
+  await service.getVersion();
 
   return service;
 }
