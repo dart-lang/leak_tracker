cc44913e2abc048416853bdf280c826fa75156dc
Polina Cherkasova <polina.c@live.com>
2023-11-10 18:48:41 -0800

    -

---
+++ pkgs/leak_tracker/lib/src/leak_tracking/primitives/_retaining_path/_retaining_path_isolate.dart
@@ -0,0 +1,43 @@
+// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:developer';
+import 'dart:isolate';
+
+import 'package:vm_service/vm_service.dart' hide Isolate;
+
+/// Returns retaining path for an object, if it can be detected.
+///
+/// If [object] is null or object reference cannot be obtained or isolate cannot be obtained,
+/// returns null.
+Future<RetainingPath?> retainingPathImpl(
+  VmService service,
+  Object? object,
+) async {
+  if (object == null) return null;
+
+  final objRef = Service.getObjectId(object);
+
+  if (objRef == null) return null;
+
+  try {
+    // ignore: sdk_version_since
+    final isolateId = Service.getIsolateId(Isolate.current);
+
+    if (isolateId == null) {
+      return null;
+    }
+
+    final result = await service.getRetainingPath(
+      isolateId,
+      objRef,
+      100000,
+    );
+
+    return result;
+  } on SentinelException {
+    return null;
+  }
+}
