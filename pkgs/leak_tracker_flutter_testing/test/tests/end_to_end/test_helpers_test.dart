04a4a8ed3b819cdc4847521247b5cfc947b3207d
Polina Cherkasova <polina.c@live.com>
2023-12-19 10:54:11 -0800

    Update test_helpers_test.dart

--- pkgs/leak_tracker_flutter_testing/test/tests/end_to_end/test_helpers_test.dart
+++ pkgs/leak_tracker_flutter_testing/test/tests/end_to_end/test_helpers_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:flutter/cupertino.dart';
-import 'package:leak_tracker/leak_tracker.dart';
 import 'package:leak_tracker_flutter_testing/leak_tracker_flutter_testing.dart';
 import 'package:leak_tracker_flutter_testing/src/test_classes.dart';
 import 'package:test/test.dart';
