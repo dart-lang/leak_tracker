2ed2000ab17c28c13201e074a639dac42ce4672b
Polina Cherkasova <polina.c@live.com>
2023-12-30 08:54:44 -0800

    -

--- tool/temp.sh
+++ tool/temp.sh
@@ -1,6 +1,9 @@
 set -ex
 
+# Contains a path to this script, relative to the directory it was called from.
+RELATIVE_PATH_TO_SCRIPT="${BASH_SOURCE[0]}"
+
 # The directory that this script is located in.
-TOOL_DIR=$(dirname "${BASH_SOURCE[0]}")
+TOOL_DIR=`dirname "${RELATIVE_PATH_TO_SCRIPT}"`
 
 echo $TOOL_DIR
