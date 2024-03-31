b0da98eacb8581b36c169bcca06ed7b3681b25e1
Polina Cherkasova <polina.c@live.com>
2024-03-31 06:15:02 -0700

    -

--- pkgs/memory_usage/lib/memory_usage.dart
+++ pkgs/memory_usage/lib/memory_usage.dart
@@ -2,5 +2,5 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/model.dart';
-export 'src/usage_tracking.dart';
+export 'src/auto_snapshotting/auto_snapshotting.dart';
+export 'src/auto_snapshotting/model.dart';
