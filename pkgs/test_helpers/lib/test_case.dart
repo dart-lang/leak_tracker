00f6d11e5d60011b578f756c72274b70d20e4fa7
Polina Cherkasova <polina.c@live.com>
2024-11-18 12:16:59 -0800

    -

---
+++ pkgs/test_helpers/lib/test_case.dart
@@ -0,0 +1,126 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:leak_tracker/leak_tracker.dart';
+import 'package:leak_tracker_testing/leak_tracker_testing.dart';
+import 'package:matcher/expect.dart';
+
+/// Signature of `pumpWidget` method.
+typedef PumpWidgetsCallback = Future<void> Function(
+  Widget widget, [
+  Duration? duration,
+]);
+
+/// Signature of `runAsync` method.
+typedef RunAsyncCallback<T> = Future<T?> Function(
+  Future<T> Function() callback,
+);
+
+/// Callback for test body, with access to Flutter specific test methods.
+typedef TestCallback = Future<void> Function(
+  PumpWidgetsCallback? pumpWidgets,
+  RunAsyncCallback<dynamic>? runAsync,
+);
+
+/// A test case to verify leak detection.
+class LeakTestCase {
+  LeakTestCase({
+    required this.name,
+    required this.body,
+    this.notDisposedTotal = 0,
+    this.notGCedTotal = 0,
+    this.notDisposedInHelpers = 0,
+    this.notGCedInHelpers = 0,
+  });
+
+  /// Name of the test.
+  final String name;
+
+  /// Test body.
+  final TestCallback body;
+
+  /// Expected number of not disposed objects.
+  final int notDisposedTotal;
+
+  /// Expected number of not GCed objects.
+  final int notGCedTotal;
+
+  /// Expected number of not disposed objects created by test helpers.
+  final int notDisposedInHelpers;
+
+  /// Expected number of not GCed objects created by test helpers.
+  final int notGCedInHelpers;
+
+  /// Verifies [leaks] contain expected leaks for the test.
+  ///
+  /// [settings] is used to determine:
+  /// * if some leaks should be ignored
+  /// * which diagnostics should be collected
+  ///
+  /// [testDescription] is used in description for the failed expectations.
+  void verifyLeaks(Leaks leaks, LeakTesting settings,
+      {required String testDescription}) {
+    final expectedContextKeys = <String>[
+      if (settings.leakDiagnosticConfig.collectStackTraceOnStart)
+        ContextKeys.startCallstack,
+    ];
+
+    _verifyLeakList(
+      testDescription,
+      LeakType.notDisposed,
+      leaks,
+      ignore: settings.ignore || settings.ignoredLeaks.notDisposed.ignoreAll,
+      expectedCount: notDisposedTotal -
+          (settings.ignoredLeaks.createdByTestHelpers
+              ? notDisposedInHelpers
+              : 0),
+      expectedContextKeys: expectedContextKeys,
+    );
+
+    // Add diagnostics that is relevant for notGCed only.
+    if (settings.leakDiagnosticConfig.collectRetainingPathForNotGCed) {
+      expectedContextKeys.add(ContextKeys.retainingPath);
+    }
+    if (settings.leakDiagnosticConfig.collectStackTraceOnDisposal) {
+      expectedContextKeys.add(ContextKeys.disposalCallstack);
+    }
+
+    _verifyLeakList(
+      testDescription,
+      LeakType.notGCed,
+      leaks,
+      ignore: settings.ignore ||
+          settings.ignoredLeaks.experimentalNotGCed.ignoreAll,
+      expectedCount: notGCedTotal -
+          (settings.ignoredLeaks.createdByTestHelpers ? notGCedInHelpers : 0),
+      expectedContextKeys: expectedContextKeys,
+    );
+  }
+
+  void _verifyLeakList(
+    String testDescription,
+    LeakType type,
+    Leaks leaks, {
+    required int expectedCount,
+    required List<String> expectedContextKeys,
+    required bool ignore,
+  }) {
+    final list = leaks.byType[type] ?? <LeakReport>[];
+
+    expect(
+      list.length,
+      ignore ? 0 : expectedCount,
+      reason: testDescription,
+    );
+
+    // Verify context keys.
+    for (final leak in list) {
+      final actualKeys = leak.context?.keys.toList() ?? <String>[];
+      expect(actualKeys..sort(), equals(expectedContextKeys..sort()),
+          reason: '$testDescription, $type');
+    }
+  }
+}
--- pkgs/web_tests/README.md
+++ pkgs/web_tests/README.md
@@ -1,5 +1,13 @@
-This package contains tests that need 'flutter_test' to run.
+This package contains web tests for leak tracker.
 
-The tests are in separate separated package:
-1. To avoid cycle dependencies between flutter_test and leak_tracker_flutter_testing.
-2. Because `flutter test --platform chrome` fails for any test folder except `web`.
+The tests are in separate package, because:
+1. `flutter test --platform chrome` fails for any test folder except `test`.
+2. 'normal' testing for leak tracker uses vm service, that is not available for web.
+3. Web testing is not possible with `package:test`, we need `package:flutter_test`,
+and there are complications because it depends on concrete version of leak_tracker_flutter_testing.
+
+Command to run tests:
+
+```
+flutter test --platform chrome
+```
